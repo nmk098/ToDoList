bd8070e33434ccb0536ff75bfd08c191bfcb42e1
nmk098 <nkuryumov@gmail.com>
2023-03-07 21:26:35 +0400

    Initial Commit

---
+++ ToDoForKsyu/ToDoForKsyuApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoForKsyuApp.swift
+//  ToDoForKsyu
+//
+//  Created by Никита Курюмов on 07.03.23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoForKsyuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
